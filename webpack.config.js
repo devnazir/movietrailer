25d218fcd1d87492e8101116f58567ca1b694e90
devnazir <nazirbachrululum@gmail.com>
2021-01-14 15:27:24 +0700

    wrong write at publicPath fonts

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
                     loader: 'file-loader',
                     options: {
                         outputPath: '../../fonts/',
-                        publicPath: '/fonts',
+                        publicPath: 'dist/fonts',
                         name: '[name]-[hash:8].[ext]'
                     }
                 }
